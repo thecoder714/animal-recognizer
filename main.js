ab05b88c3e526b9abdd0216be3ad5c3a4faff53c
thecoder714 <75258277+thecoder714@users.noreply.github.com>
2022-01-15 22:40:39 +0530

    v2 done

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-function startDetection() {}
+function startDetection() {
+    navigator.getUserMedia({audio: true});
+    ml5.soundClassifier('#/model.json', compare());
+}
+
+function compare() {
+    classifier.classify(resultsAnalyzer);
+    resultsAnalyzer();
+}
+
+function resultsAnalyzer(err, results) {
+    if (err) {
+        console.error(err);
+    } else {
+        document.querySelector('#results').innerHTML = undefined;
+    }
+}
